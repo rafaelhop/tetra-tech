56d395f45c99a7d4b70d044b098487127fe19a93
Rafael Souza <rafaelhop@hotmail.com>
2020-12-16 18:07:11 -0300

    build project

---
+++ desafio-tetra/src/main/java/com/tetra/desafiotetra/DesafioTetraApplication.java
@@ -0,0 +1,13 @@
+package com.tetra.desafiotetra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTetraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTetraApplication.class, args);
+	}
+
+}
---
+++ desafio-tetra/src/test/java/com/tetra/desafiotetra/DesafioTetraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tetra.desafiotetra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTetraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
